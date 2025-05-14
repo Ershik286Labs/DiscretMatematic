754cdf8356b3a404af3fbe2d9972d39e57c75d04
Ershik286Labs <ershov.ershik286@gmail.com>
2025-05-14 18:43:55 +0300

    Add files via upload

---
+++ DisckLab5.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void printMatrix(vector<vector<int>>& matrix) {
+    cout << " |";
+    for (int i = 0; i < matrix[0].size(); i++) {
+        cout << " " << i + 1 << "|";
+    }
+    cout << endl;
+    for (int i = 0; i < matrix.size(); i++) {
+        cout << i + 1 << "|";
+        for (int j = 0; j < matrix[i].size(); j++) {
+            if (matrix[i][j] < 0)
+                cout << matrix[i][j] << "|";
+            else cout << " " << matrix[i][j] << "|";
+        }
+        cout << endl;
+    }
+}
+
+// Функция для умножения двух матриц
+vector<vector<int>> multiplyMatrices(const vector<vector
+    <int>>&A, const vector<vector<int>>& B) {
+    int n = A.size();
+    vector<vector<int>> C(n, vector<int>(n, 0));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k < n; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    return C;
+}
+
+// Функция для возведения матрицы в степень (бинарное возведение в степень)
+vector<vector<int>> matrixPower(vector<vector
+    <int>> base, int exponent) {
+    int n = base.size();
+    vector<vector<int>> result(n, vector<int>(n, 0));
+
+    // Единичная матрица
+    for (int i = 0; i < n; ++i) {
+        result[i][i] = 1;
+    }
+
+    while (exponent > 0) {
+        if (exponent % 2 == 1) {
+            result = multiplyMatrices(result, base);
+        }
+        base = multiplyMatrices(base, base);
+        exponent /= 2;
+    }
+
+    return result;
+}
+
+int main() {
+    string filename;
+    int pathLength;
+
+    cout << "Введите имя файла с матрицей инцидентности: ";
+    cin >> filename;
+    cout << "Введите длину пути: ";
+    cin >> pathLength;
+
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Не удалось открыть файл" << endl;
+        return 1;
+    }
+
+    int n, m;
+    file >> n >> m;
+
+  /*  if (n < 5 || m < 5) {
+        cerr << "Матрица должна содержать как минимум 5 вершин и 5 дуг." << endl;
+            return 1;
+    }*/
+
+    vector<vector<int>> incidenceMatrix(n, vector<int>(m));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            file >> incidenceMatrix[i][j];
+        }
+    }
+    cout << "Матрица инцидентностности: " << endl;
+
+    printMatrix(incidenceMatrix);
+
+    cout << endl;
+    file.close();
+    // Преобразование матрицы инцидентности в матрицу смежности
+    vector<vector<int>> adjacencyMatrix(n, vector<int>(n, 0));
+    for (int j = 0; j < m; ++j) {
+        int from = -1, to = -1;
+        for (int i = 0; i < n; ++i) {
+            if (incidenceMatrix[i][j] == -1) {
+                from = i;
+            }
+            else if (incidenceMatrix[i][j] == 1) {
+                to = i;
+            }
+        }
+        if (from != -1 && to != -1) {
+            adjacencyMatrix[to][from] = 1;
+        }
+    }
+    cout << "Матрица смежности: " << endl;
+    printMatrix(adjacencyMatrix);
+
+    // Возведение матрицы смежности в степень pathLength
+    vector<vector<int>> poweredMatrix =
+        matrixPower(adjacencyMatrix, pathLength);
+
+    cout << "\nМатрица смежности возведенная в степень: " << endl;
+    printMatrix(poweredMatrix);
+    // Вычисление общего количества путей
+    int totalPaths = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            totalPaths += poweredMatrix[i][j];
+        }
+    }
+    cout << "Общее количество путей длины " <<
+        pathLength << ": " << totalPaths << endl;
+}
---
+++ Discklab4.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int lineOneMatrix = 0;
+int colomnOneMatrix = 0;
+int lineTwoMatrix = 0;
+int colomnTwoMatrix = 0;
+
+void printMatrix(int** matrix, int lines, int columns) {
+    for (int x = 0; x < columns; x++) {
+        int k = 0;
+        for (int y = 0; y < lines; y++) {
+            if (matrix[y][x] == 0) {
+                k++;
+            }
+        }
+        if (k == lines) {
+            for (int y = 0; y < lines; y++) {
+                matrix[y][x] = 5;
+            }
+        }
+    }
+    for (int y = 0; y < lines; y++) {
+        cout << " " << y + 1 << "|";
+        for (int x = 0; x < columns; x++) {
+            int number = matrix[y][x];
+            if (number == 5) {
+                continue;
+            }
+            if (number >= 0) cout << " " << number << " ";
+            else cout << number << " ";
+        }
+        cout << endl;
+    }
+}
+
+int** createDifferenceGraphMatrix(int** matrix1, int** matrix2, int lines, int columns) {
+    int** diffMatrix = new int* [lines];
+    for (int i = 0; i < lines; i++) {
+        diffMatrix[i] = new int[columns];
+        for (int j = 0; j < columns; j++) {
+            // Ребро есть в первой матрице и его нет во второй (значения разные)
+            if (matrix1[i][j] != 0) {
+                int check = matrix1[i][j];
+                int len = i;
+                int col = j;
+                diffMatrix[i][j] = check;
+                for (int y = 0; y < lines; y++) {
+                    if (matrix1[y][j] == check * -1) {
+                        int len1 = y;
+                        if (matrix2[len][j] == check && matrix2[len1][j] == check * -1) {
+                            diffMatrix[i][j] = 0;
+                        }
+                        else {
+                            diffMatrix[i][j] = check;
+                        }
+                        break;
+                    }
+                }
+
+            }
+            else diffMatrix[i][j] = 0;
+        }
+    }
+    return diffMatrix;
+}
+
+void freeMatrix(int** matrix, int lines) {
+    for (int i = 0; i < lines; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+
+int main() {
+    fstream OneMatrixFile("Исходная первая М-ца инциндетности Ершов Никита 1304.txt", ios::in);
+    fstream TwoMatrixFile("Исходная вторая М-ца инциндетности Ершов Никита 1304.txt", ios::in);
+
+
+    OneMatrixFile >> lineOneMatrix;
+    OneMatrixFile >> colomnOneMatrix;
+    TwoMatrixFile >> lineTwoMatrix;
+    TwoMatrixFile >> colomnTwoMatrix;
+
+    // Чтение первой матрицы
+    int** matrixOne = new int* [lineOneMatrix];
+    for (int i = 0; i < lineOneMatrix; i++) {
+        matrixOne[i] = new int[colomnOneMatrix];
+        for (int j = 0; j < colomnOneMatrix; j++) {
+            OneMatrixFile >> matrixOne[i][j];
+        }
+    }
+
+    // Чтение второй матрицы
+    int** matrixTwo = new int* [lineTwoMatrix];
+    for (int i = 0; i < lineTwoMatrix; i++) {
+        matrixTwo[i] = new int[colomnTwoMatrix];
+        for (int j = 0; j < colomnTwoMatrix; j++) {
+            TwoMatrixFile >> matrixTwo[i][j];
+        }
+    }
+
+    cout << "Первая матрица инцидентности:" << endl << endl;
+    printMatrix(matrixOne, lineOneMatrix, colomnOneMatrix);
+    cout << endl;
+
+    cout << "Вторая матрица инцидентности:" << endl << endl;
+    printMatrix(matrixTwo, lineTwoMatrix, colomnTwoMatrix);
+    cout << endl;
+
+    // Создание матрицы разностного графа
+    int** differenceGraphMatrix = createDifferenceGraphMatrix(matrixOne, matrixTwo, lineOneMatrix, colomnOneMatrix);
+
+    cout << "Матрица инциндентности разностного графа:" << endl << endl;
+    printMatrix(differenceGraphMatrix, lineOneMatrix, colomnOneMatrix);
+    cout << endl;
+
+    return 0;
+}
